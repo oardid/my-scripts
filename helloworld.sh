ddc5469f2d1f461084a9d961fa8d67fd729dbc9e
Omar <omar.ardid.817@gmail.com>
2024-02-20 17:41:43 -0600

    Adding helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  ops201-challenged-my-first-bash-script
+# Author:                       Omar Ardid
+# Date of latest revision:      02/20/2024
+# Purpose:                      Print a string to the terminal
+
+# Declaration of variables
+
+perfect="I am working perfectly!"
+
+# Declaration of functions
+
+
+# Main
+
+echo "Hello World"
+echo $perfect
+
+# End
