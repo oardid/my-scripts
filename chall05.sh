0c21a2231cf2ec9146e49a4638739a638fb78361
Omar <omar.ardid.817@gmail.com>
2024-02-24 21:55:22 -0600

    Edited line 21, had two 4490 in choice

--- chall05.sh
+++ chall05.sh
@@ -11,15 +11,12 @@
 echo "Your current PID is: $$"
 #Loops until user crtl+c
 while true; do
-    read -p "Enter your PID to kill the process or stay here forever. If you give up crtl+c to quit: " choice
+    read -p "Enter your PID to kill the process or crtl+c to quit: " choice
     if [[ -z "$choice" ]]; then 
     # If crtl+c is entered, break out of loop
         break
     fi
     #Kill procces with PID
     echo "Killing process with PID: $$"
-    kill "$$choice" 2>/dev/null || echo "Failed to kill process with PID: $choice"
+    kill "$choice" 2>/dev/null || echo "Failed to kill process with PID: $choice"
 done
-
-
-
