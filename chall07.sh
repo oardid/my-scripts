b327176c58918c03ddcb66339c59bfdef135901c
Omar <omar.ardid.817@gmail.com>
2024-02-27 19:15:28 -0600

    Making a script that use lshw to display system information

---
+++ chall07.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+# Script Name:                  ops201-challenge07-System Information
+# Author:                       Omar Ardid
+# Date of latest revision:      02/27/2024
+# Purpose:                      Create a script that uses lshw to display system information
+# Resources:                    
+
+
+
+sys_info(){
+    read -p "Do you want to display Computer name? (y/n) : " answer
+    if [[ $answer == "y" ]]; then 
+    echo "Name of computer: "$(lshw | grep "" -m1)
+    fi
+    read -p "Do you want to display CPU? (y/n) : " answer
+    if [[ $answer == "y" ]]; then
+    echo "CPU: "$(lshw | grep "*-cpu" -A 6)
+    fi
+    read -p "Do you want to display RAM? (y/n) : " answer
+    if [[ $answer == "y" ]]; then
+    echo "RAM:"$(lshw -class memory | grep -E 'description:|physical id:|size:')
+    fi
+    read -p "Do you want to display 'Display adapter'? (y/n) : " answer
+    if [[ $answer == "y" ]]; then
+    echo "Display adapter: "$(lshw -class display | grep -E 'description:|product:|vendor:|physical id:|bus info:|width:|clock:|capabilities:|configuration:|resources:')
+    fi
+    read -p "Do you want to display Network adapter? (y/n) : " answer
+    if [[ $answer == "y" ]]; then
+    echo "Network adapter: "$(lshw -class network | grep -E 'description:|product:|vendor:|physical id:|bus info:|logical name:|version:|serial:|size:|capacity:|width:|clock:|capabilities:|configuration:|resources:')
+    done
+}
+sys_info
+
