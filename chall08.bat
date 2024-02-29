92ee1d225b7b81ca2ffd1adb33a8a6f947ee74ca
Omar D Ardid <158124623+oardid@users.noreply.github.com>
2024-02-28 22:06:01 -0600

    Create chall08.bat

---
+++ chall08.bat
@@ -0,0 +1,17 @@
+@echo off 
+
+set /p source_folder=Enter the source folder path:
+set /p dest_folder=Enter the destination folder path:
+
+if not exist %source_folder% (
+	echo Error: Source folder doesn't exist.
+	goto :eof
+)
+
+if not exist %source_destination% (
+	echo Error: Destination folder doesn't exist
+	goto :eof
+)
+
+robocopy %source_folder% %source_destination% /R
+:end
