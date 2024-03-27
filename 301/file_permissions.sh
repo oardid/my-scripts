222947b4993edc59c53209007f289e24b648b28f
Omar <omar.ardid.817@gmail.com>
2024-03-27 16:01:54 -0500

    update

---
+++ 301/file_permissions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script Name:                  ops301-challenge03
+# Author:                       Omar Ardid
+# Date of latest revision:      03/27/2024
+# Purpose:                      
+
+# Declaration of variables
