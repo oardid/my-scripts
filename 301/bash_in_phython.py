d931fff73e0f2d63f3989976a5b67f463cfd2dc3
Omar <omar.ardid.817@gmail.com>
2024-04-01 20:45:12 -0500

    Updated

---
+++ 301/bash_in_phython.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+# Script Name:                  ops301-challenge06
+# Author:                       Omar Ardid
+# Date of latest revision:      04/01/2024
+# Purpose:                      Execut bash commands inside python
+
+import os 
+os.system("whoami")
+os.system("ip a")
+os.system("lshw -short")
