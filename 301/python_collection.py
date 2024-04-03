ed583a58a6faa82b6eecbad47c31cd535248c805
Omar <omar.ardid.817@gmail.com>
2024-04-03 16:15:59 -0500

    Created working lists in python

---
+++ 301/python_collection.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Script Name:                  ops301-challenge08
+# Author:                       Omar Ardid
+# Date of latest revision:      04/03/2024
+# Purpose:                      Working with lists in Python
+
+# Variable list of ten string elements
+print("")
+foods = ["Pizza", "Hotdog", "Corn", "Chicken", "Burger", "Waffles", "Salads", "Pasta", "Sandwich", "Eggs"]
+print("Food list: " + str(foods))
+
+# Printing the fourth item in the list
+print("Fourth item in the list: " + foods[3])
+
+# Print the sixth item in the list
+print("Sixth item in the list: " + foods[5])
+
+# Changing the value of the seventh item to onion
+foods.remove("Salads")
+foods.insert(6, "Onion")
+print(foods)
+print("Replaced 'Salads' to 'Onion' from food list: " + foods[6])
