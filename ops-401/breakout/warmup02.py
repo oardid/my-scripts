bb6cc8624e666ebff0ac7c74aa43ad1cc6bdf640
Omar <omar.ardid.817@gmail.com>
2024-04-30 14:52:28 -0500

    created breakout and demos directories

---
+++ ops-401/breakout/warmup02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+num = int(input("Enter a number: "))
+text = input("Enter a text to print out: ")
+
+for _ in range(num): 
+    print (text)
---
+++ ops-401/demos/demo02.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+# TODO Practice creating a function, calling the function, and passing a variable to the function
+
+#def my_function(my_list):
+#    print(my_list)
+
+# TODO Set an Optional Argument List for the function
+
+#def my_function(my_list = ["a", "b", "c"]):
+#    print(my_list)
+
+# TODO Loop through the Argument List
+
+#def my_function(my_list = ["a", "b", "c"]):
+#    for element in my_list:
+#        print(element)
+
+# TODO Using the Library datetime, print out the current date and time
+
+#import datetime
+#from datetime import date
+
+#today = datetime.date.today()
+#print(today)
+
+# TODO Using the Library datetime, override the exisiting date
+# TODO Using the time module, print out the current time
+
+#import time
+
+#now = time.time.now()
+#print(now)
+
+# TODO Using the time module, print out the current time
+# TODO Using the time module, pause the script for 5 seconds
+
+#import time
+#time.sleep(5)
+#print("Did I pause for 5 seconds?")
+
+# TODO Using the OS module, ping the localhost 2 times
+
+import os
+result = os.system('ping localhost -c 2')
+
+#if __name__ == "__main__":
+#    my_variable = [1,2,3,4,5]
+#    my_function
