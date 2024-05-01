7cb71d870b8cb42c7d9e043cd5ad3623bc15adda
Omar <omar.ardid.817@gmail.com>
2024-05-01 12:35:16 -0500

    Updated script

--- ops-401/uptime_sensor_tool_part1.py
+++ ops-401/uptime_sensor_tool_part1.py
@@ -6,22 +6,27 @@
 # Purpose:                      Create an uptime sensor tool that uses ICMP packets to evaluate if hosts on the LAN are up or down.
 
 import os 
+import time
+from datetime import datetime
 
 # Transmit a single ICMP (ping) packet to a specific IP every two seconds.
 ip_address = input("Enter an ip address: ")
-result = os.system(f'ping {ip_address} -c 2')
-
-# Get the current timestamp
-from datetime import datetime
-timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+result = os.system(f'ping -c 1 {ip_address} > /dev/null 2>&1')
 
 # Evaluate the response as either success or failure.
 # Assign success or failure to a status variable.
-if result == 0:
-    status = "Success"
-    print(f"{timestamp} Destination IP: {ip_address} Lan is up and running! ")
-else:
-    status = "Failure"
-    print(f"{timestamp} Destination IP: {ip_address} Lan is down!")
-
-print(f"Status: {status}")
+pings = 0 
+while True:
+    # Get the current timestamp
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    if result == 0:
+        status = "Success"
+        print(f"{timestamp} Network {status} to {ip_address} Lan is up and running! ")
+        time.sleep(2)
+    else:
+        status = "Failure"
+        print(f"{timestamp} Network {status} to {ip_address} Lan is down!")
+        time.sleep(2)
+    pings += 1
+    if pings >= 4:
+        break
