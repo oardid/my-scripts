43e8aad73c2838acb008eb980209bb8aa79eade8
Omar <omar.ardid.817@gmail.com>
2024-05-12 13:04:37 -0500

    update

--- ops-401/autoconfigsettings.ps1
+++ ops-401/autoconfigsettings.ps1
@@ -1,8 +1,5 @@
-# 1.1.5(L1) Set 'Do not allow anonymous enumeration of SAM accounts and shares' policy
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymousSAM" -Value 1
+# Configure Minimum password length (1.1.5 L1)
+Set-LocalUser -Name "*" -MinimumPasswordLength 14
 
-# 18.4.3(L1) Set 'Enable insecure guest logons' to Disabled
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "EnableInsecureGuestLogons" -Value 0
-
-# Restart the Server service to apply changes
-Restart-Service -Name LanmanServer
+# Configure SMB v1 client driver (18.4.3 L1)
+Set-SmbClientConfiguration -EnableSMB1Protocol $false
